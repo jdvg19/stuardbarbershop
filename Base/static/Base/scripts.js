30a8f728bd519012439b686922f1fce96fd9a53f
Amadeux17 <alfredoloyocarias02@gmail.com>
2024-10-09 20:46:10 -0600

    script configuration

--- Base/static/Base/scripts.js
+++ Base/static/Base/scripts.js
@@ -5,12 +5,12 @@ function openNav() {
   if (anchoVentana < 768) {
     document.getElementById("mySidenav").style.width = "100%";
     document.getElementById("openBtn").style.hidden = true;
-    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("main").style.marginLeft = "12em";
 
   } else {
-    document.getElementById("mySidenav").style.width = "15%";
+    document.getElementById("mySidenav").style.width = "12em";
     document.getElementById("openBtn").style.hidden = true;
-    document.getElementById("main").style.marginLeft = "15%";
+    document.getElementById("main").style.marginLeft = "12em";
   }
 
 }
