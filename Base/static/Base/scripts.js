80be5b30b7824cfa665b4da887a4a5f3ca1c7f85
Amadeux17 <alfredoloyocarias02@gmail.com>
2024-10-09 19:54:20 -0600

    Menu mobile

--- Base/static/Base/scripts.js
+++ Base/static/Base/scripts.js
@@ -1,11 +1,22 @@
-function openNav(){
-  document.getElementById("mySidenav").style.width="15%";
-  document.getElementById("openBtn").style.marginLeft="15%";
-  document.getElementById("main").style.marginLeft="15%";
+let anchoVentana = window.innerWidth
+
+function openNav() {
+
+  if (anchoVentana < 768) {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("openBtn").style.hidden = true;
+    document.getElementById("main").style.marginLeft = "25%";
+
+  } else {
+    document.getElementById("mySidenav").style.width = "15%";
+    document.getElementById("openBtn").style.hidden = true;
+    document.getElementById("main").style.marginLeft = "15%";
+  }
+
 }
 
-function closeNav(){
-  document.getElementById("mySidenav").style.width="0";
-  document.getElementById("openBtn").style.marginLeft="0";
-  document.getElementById("main").style.marginLeft="0";
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("openBtn").style.marginLeft = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
