13aefc6d1ecb3d16d5d993c3ada47e16bfa2fe7d
Amadeux17 <alfredoloyocarias02@gmail.com>
2024-10-28 18:52:20 -0600

    error citas forms arreglado

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbbarbershop',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Alfca_017',
         'HOST': 'localhost',
         'PORT': '3306',
     }
